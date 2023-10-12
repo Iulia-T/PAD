97d98393bcc3de2ffa4abe5e5607054d68361838
Iulia-T <72933695+Iulia-T@users.noreply.github.com>
2023-10-12 15:52:20 +0300

    Add files via upload

--- order_service.py
+++ order_service.py
@@ -1,24 +1,77 @@
-from flask import Flask, Request
-from flask_restful import Resource, Api
+import requests
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+PRODUCT_SERVICE_URL = "http://localhost:5001" 
+
+orders = {}
+
+@app.route('/orders', methods=['POST'])
+def create_order():
+    data = request.get_json()
+    product_id = data.get('product_id')
+    
+    product_response = requests.get(f"{PRODUCT_SERVICE_URL}/products/{product_id}")
+    
+    if product_response.status_code == 200:
+        product_data = product_response.json()
+        order_id = len(orders) + 1  
+        order = {
+            "id": order_id,
+            "product_id": product_id,
+            "product_name": product_data['name'],
+            "product_price": product_data['price'],
+        }
+        orders[order_id] = order
+        return jsonify({"message": "Order created", "order": order}), 201
+    else:
+        return jsonify({"error": "Product not found"}), 404
+
+@app.route('/orders/<int:order_id>', methods=['GET'])
+def get_order(order_id):
+    order = orders.get(order_id)
+    if order:
+        return jsonify(order)
+    else:
+        return jsonify({"error": "Order not found"}), 404
+
+@app.route('/orders/<int:order_id>', methods=['PUT'])
+def update_order(order_id):
+    data = request.get_json()
+    order = orders.get(order_id)
+    if order:
+        product_id = data.get('product_id', order['product_id'])
+        
+        product_response = requests.get(f"{PRODUCT_SERVICE_URL}/products/{product_id}")
+        
+        if product_response.status_code == 200:
+            product_data = product_response.json()
+            order['product_id'] = product_id
+            order['product_name'] = product_data['name']
+            order['product_price'] = product_data['price']
+            return jsonify({"message": "Order updated", "order": order})
+        else:
+            return jsonify({"error": "Product not found"}), 404
+    else:
+        return jsonify({"error": "Order not found"}), 404
+
+
+@app.route('/orders/<int:order_id>', methods=['DELETE'])
+def cancel_order(order_id):
+    order = orders.pop(order_id, None)
+    if order:
+        return jsonify({"message": "Order canceled", "order": order})
+    else:
+        return jsonify({"error": "Order not found"}), 404
+
+if __name__ == '__main__':
+    app.run(port=5002)
 
 
 
-order_data = {
-    "dish_name": "dish1", 
-    "quantity": 3, 
-}
-
-# URL of the Stock microservice
-stock_url = "http://localhost:5000/stock"  
-
-# Make an HTTP POST request to the Stock microservice
-response = Request.post(stock_url, json=order_data)
-
-# Check the response from the Stock microservice
-if response.status_code == 200:
-    updated_stock_data = response.json()
-    print("Stock updated successfully:")
-    print(updated_stock_data)
-else:
-    print("Failed to update stock")
+#http POST http://localhost:5002/orders product_id:=2
+#http GET http://localhost:5002/orders/1
+#http PUT http://localhost:5002/orders/1 product_id:=3
+#http DELETE http://localhost:5002/orders/1
 
